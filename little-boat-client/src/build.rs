67ec9f31582b432da3a5603f8d355e815e3ae4c8
Hash2000 <infinite_2000@mail.ru>
2025-07-19 17:26:34 +0300

    settings for proof to concept configuration

--- little-boat-client/src/build.rs
+++ little-boat-client/src/build.rs
@@ -1,3 +1,4 @@
 fn main() {
+    #[cfg(feature = "slint-ui")]
     slint_build::compile("src/ui/Application.slint").expect("Slint build failed");
 }
--- little-boat-client/src/main.rs
+++ little-boat-client/src/main.rs
@@ -1,14 +1,30 @@
+use anyhow::Result;
 
+#[cfg(feature = "slint-ui")]
 slint::include_modules!();
 
-use std::error::Error;
+fn main() -> Result<()> {
+  #[cfg(feature = "slint-ui")]
+  return Ok(main_full_featured()?);
 
-fn main() -> Result<(), Box<dyn Error>> {
-    let ui = ApplicationWindow::new()?;
+  #[cfg(feature = "console-ui")]
+  return Ok(main_poc_featured()?);
 
+  #[cfg(not(any(feature = "slint-ui", feature = "console-ui")))]
+  Ok(())
+}
 
+#[cfg(feature = "slint-ui")]
+#[tokio::main]
+async fn main_full_featured() -> Result<()> {
+  let ui = ApplicationWindow::new()?;
 
-    ui.run()?;
+  ui.run()?;
+  Ok(())
+}
 
-    Ok(())
+#[cfg(feature = "console-ui")]
+#[tokio::main]
+async fn main_poc_featured() -> Result<()> {
+  Ok(())
 }
