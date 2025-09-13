17e4148a9da096b3c96811de287edc64b2884285
Hash2000 <infinite_2000@mail.ru>
2025-09-13 13:04:46 +0300

    update code

--- little-boat-client/src/controllers/chat_view_controller.rs
+++ little-boat-client/src/controllers/chat_view_controller.rs
@@ -1,54 +1,41 @@
 use std::rc::Rc;
+use std::sync::Arc;
 
-use slint::{ComponentHandle, Model, SharedString};
+use slint::{ComponentHandle, SharedString};
 
 use crate::ui::ApplicationWindow;
-use crate::ui::{ChatViewAdapter, ChatMessage, ChatMessageFormat};
+use crate::ui::{ChatMessage, ChatMessageFormat, ChatViewAdapter};
 
-
-pub struct ChatViewController {
+pub struct Controller {
   messages: Rc<slint::VecModel<ChatMessage>>,
 }
 
-pub fn init(app: &ApplicationWindow) {
+impl Controller {
+  pub fn new() -> Self {
+    Self { messages: Rc::new(slint::VecModel::from(vec![])) }
+  }
+}
 
-  let app_weak = app.as_weak();
-  // app_weak.upgrade_in_event_loop(move |window| {
-  //   let adapter = window.global::<ChatViewAdapter>();
-  //   let messages : Vec<ChatMessage> = adapter.get_messages().iter().collect();
+pub fn init(app: &ApplicationWindow, controller: Arc<Controller>) {
+    let app_weak = app.as_weak();
+    // app_weak.upgrade_in_event_loop(move |window| {
 
-  //   let mut message = Rc::new(ChatMessage::default());
-  //   message.content = SharedString::from(msg);
-  //   message.id = SharedString::from("testuid");
-  //   message.sender = SharedString::from("senderuid");
-  //   message.format = ChatMessageFormat::Markdown;
+    // });
 
-  //   messages.push(message);
+    app.global::<ChatViewAdapter>().on_message(move |msg| {
+      let bindapp = app_weak.unwrap();
+      add_message_into_list(&bindapp, &controller, msg);
+    });
+}
 
-  // });
+fn add_message_into_list(app: &ApplicationWindow, controller: &Controller, msg: SharedString) {
+  let mut message = ChatMessage::default();
+  message.content = SharedString::from(msg);
+  message.id = SharedString::from("testuid");
+  message.sender = SharedString::from("senderuid");
+  message.format = ChatMessageFormat::Markdown;
 
+  controller.messages.push(message);
 
-  app.global::<ChatViewAdapter>().on_message(move|msg| {
-    let mut message = ChatMessage::default();
-    message.content = SharedString::from(msg);
-    message.id = SharedString::from("testuid");
-    message.sender = SharedString::from("senderuid");
-    message.format = ChatMessageFormat::Markdown;
-
-    let binding = app_weak.unwrap();
-    let adapter = binding.global::<ChatViewAdapter>();
-
-    let mut messages: Vec<ChatMessage> = adapter.get_messages().iter().collect();
-    messages.push(message);
-
-    let controller = ChatViewController {
-      messages: Rc::new(slint::VecModel::from(messages))
-    };
-
-    adapter.set_messages(controller.messages.clone().into());
-  });
-
-
-
-
+  app.global::<ChatViewAdapter>().set_messages(controller.messages.clone().into());
 }
--- little-boat-client/src/lib.rs
+++ little-boat-client/src/lib.rs
@@ -1,7 +1,10 @@
+use little_boat_core::run_client_app;
 
 pub mod ui {
   slint::include_modules!();
 }
+
+use std::sync::Arc;
 
 use slint::*;
 
@@ -12,8 +15,15 @@ pub mod controllers {
 use controllers::*;
 
 pub async fn run_app() -> anyhow::Result<()> {
+  // tokio::spawn(async move {
+  //   if let Err(e) = run_client_app().await {
+  //     eprintln!("Client app error: {}", e);
+  //   }
+  // });
+
   let app = ui::ApplicationWindow::new()?;
-  chat_view_controller::init(&app);
+  let chat_view = Arc::new(chat_view_controller::Controller::new());
+  chat_view_controller::init(&app, chat_view.clone());
 
   app.run()?;
   Ok(())
