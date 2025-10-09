19fdaffbc14c9c1fa81bf1f65bc8b446a39f8b9c
Hash2000 <infinite_2000@mail.ru>
2025-10-09 12:08:14 +0300

    iced ampty App

--- little-boat-client/src/lib.rs
+++ little-boat-client/src/lib.rs
@@ -1,9 +1,77 @@
+use iced::event::{self, Event};
+use iced::{keyboard, window};
+use iced::keyboard::key;
+use iced::widget::{
+  button, center, column, container, mouse_area, opaque, operation, pick_list, row, space, stack,
+  text, text_input,
+};
+use iced::{Bottom, Color, Element, Fill, Subscription, Task};
 
+use std::fmt;
 
 pub fn run_app() -> anyhow::Result<()> {
-  env_logger::Builder::from_env(env_logger::Env::default().default_filter_or("info")).init();
 
-
+  iced::daemon(App::default, App::update, App::view)
+    .subscription(App::subscription)
+    .title()
+    .run()?;
 
   Ok(())
+}
+
+#[derive(Default)]
+struct App {
+  show_modal: bool,
+}
+
+#[derive(Debug, Clone)]
+enum Message {
+  Event(Event),
+}
+
+impl App {
+  fn subscription(&self) -> Subscription<Message> {
+    event::listen().map(Message::Event)
+  }
+
+  fn title(&self, _window_id: window::Id) -> String {
+    String::from("Little Boat")
+  }
+
+  fn update(&mut self, message: Message) -> Task<Message> {
+    Task::none()
+  }
+
+  fn view(&self, id: window::Id) -> Element<'_, Message> {
+    space().into()
+  }
+}
+
+impl App {
+  fn hide_modal(&mut self) {
+    self.show_modal = false;
+  }
+}
+
+fn modal<'a, Message>(
+  base: impl Into<Element<'a, Message>>,
+  content: impl Into<Element<'a, Message>>,
+  on_blur: Message,
+) -> Element<'a, Message>
+where
+  Message: Clone + 'a,
+{
+  stack![
+    base.into(),
+    opaque(
+      mouse_area(center(opaque(content)).style(|_theme| {
+        container::Style {
+          background: Some(Color { a: 0.8, ..Color::BLACK }.into()),
+          ..container::Style::default()
+        }
+      }))
+      .on_press(on_blur)
+    )
+  ]
+  .into()
 }
--- little-boat-client/src/main.rs
+++ little-boat-client/src/main.rs
@@ -1,5 +1,15 @@
+use little_boat_client::run_app;
+
 
 fn main() -> anyhow::Result<()> {
 
+  let config = env_logger::Env::default()
+    .default_filter_or("info");
+
+  env_logger::Builder::from_env(config)
+    .init();
+
+  run_app()?;
+  
   Ok(())
 }
